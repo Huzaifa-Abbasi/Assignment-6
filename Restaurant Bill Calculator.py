cc87752de5b54c7a118d21e2f21372afdf09923d
Huzaifa Abbasi <151647824+Huzaifa-Abbasi@users.noreply.github.com>
2024-01-11 23:58:14 +0500

    Update Restaurant Bill Calculator.py

--- Restaurant Bill Calculator.py
+++ Restaurant Bill Calculator.py
@@ -1,8 +1,7 @@
-#Design a function that takes the ordered items, quantities, and prices to calculate 
-#the total bill amount.
+#Design a function that takes the ordered items, quantities, and prices to calculate the total bill amount.
 #Include options for applying discounts, taxes, and splitting the bill among friends.
 
-def calculate_total_amount(items, quantities, prices,applying_discount, taxes, splitting_bill):
+def calculate_total_amount(quantities, prices,applying_discount, taxes, splitting_bill):
 
     if applying_discount:
         prices *= 0.9
@@ -11,17 +10,16 @@ def calculate_total_amount(items, quantities, prices,applying_discount, taxes, s
 
     if splitting_bill:
         prices /= 2
-    return items*prices*quantities+taxes
+    return prices*quantities+taxes
 
         
 
 taxes = 0.01
-items = int(input("Enter The Ordered Items? "))
 quantities = int(input("Enter Quantities? "))
 prices = int(input("Enter the Prices? "))
 applying_discount = (input("want to apply Discounts Yes/No? ")) == 'yes'
 splitting_bill = input("split the bill Yes/No? ") == "yes"
-total_bill = calculate_total_amount(items, quantities, prices, applying_discount, taxes, splitting_bill )
+total_bill = calculate_total_amount(quantities, prices, applying_discount, taxes, splitting_bill )
 if splitting_bill == True:
     print("Your Total Bill perperson is " ,{total_bill})
 else:
